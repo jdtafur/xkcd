042c9aa08c880ea4b6a27d0dd5937af938978427
jdtafur <juandavidtafurrangel@gmail.com>
2021-12-04 14:06:02 -0500

    basic model: comic component

--- src/app/comics/comics.module.ts
+++ src/app/comics/comics.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ComicsRoutingModule} from "./comics-routing.module";
 import {ComicsComponent} from "./comics.component";
+import { ComicComponent } from './components/comic/comic.component';
 
 @NgModule({
-  declarations: [ComicsComponent],
+  declarations: [ComicsComponent, ComicComponent],
   imports: [
     CommonModule,
     ComicsRoutingModule
---
+++ src/app/comics/components/comic/comic.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-comic',
+  templateUrl: './comic.component.html',
+  styleUrls: ['./comic.component.css']
+})
+export class ComicComponent implements OnInit {
+
+  @Input() comic: any = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
